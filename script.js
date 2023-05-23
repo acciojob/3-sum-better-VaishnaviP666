f27bd9da56ab99033b1e09ebfb142dba45eb0d5a
VaishnaviP666 <122201508+VaishnaviP666@users.noreply.github.com>
2023-05-23 14:51:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	var ar = [];
+	for(let i=0;i<arr.length;i++)
+		{
+			ar.push(arr[i]+arr[i+1]+arr[i+2]);
+		}
+  ar.sort();
+	return ar[0];
 }
 
 module.exports = threeSum;
